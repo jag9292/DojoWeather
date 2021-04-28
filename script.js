e9efafb6c6815a1813bf7fb83034649014a54c8a
Jared Green <jag9292@yahoo.com>
2021-04-28 09:51:47 -0400

    first

---
+++ script.js
@@ -0,0 +1,12 @@
+// function alert() {
+//     console.log("Loading weather report...");
+// }
+
+var cookie = document.querySelector("#removeCookie").innerHTML;
+function remove() {
+    document.querySelector("#removeCookie").innerHTML ="";
+}
+
+function celsius() {
+    console.log("celsius");
+}
